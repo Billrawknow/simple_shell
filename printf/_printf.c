242c3e763ad03abb59804edec10b405be442faff
Billrawknow <bildadronoh@gmail.com>
2023-07-25 11:07:59 -0700

    files

---
+++ printf/_printf.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string containing directives
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
+int _printf(const char *format, ...)
+{
+    /* Your implementation here */
+    return (0);
+}
