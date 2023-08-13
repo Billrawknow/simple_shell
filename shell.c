89d5d28c3ed06c34a5c0d4aba3c953b7d2b26122
Billrawknow <bildadronoh@gmail.com>
2023-08-13 07:11:50 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
 #define MAX_COMMAND_LENGTH 100
 
@@ -11,34 +8,23 @@ int main() {
     char command[MAX_COMMAND_LENGTH];
 
     while (1) {
-        printf("#cisfun$ "); // Display prompt
-        fflush(stdout); // Flush the output buffer
+        printf("#cisfun$ ");
+        fflush(stdout);
 
         if (fgets(command, sizeof(command), stdin) == NULL) {
-            // Handle end of file (Ctrl+D)
             printf("\nExiting the shell...\n");
             break;
         }
 
-        // Remove the newline character
-        command[strcspn(command, "\n")] = '\0';
+        command[strcspn(command, "\n")] = '\0'; // Remove newline character
 
-        pid_t pid = fork();
-        if (pid == 0) {
-            // Child process
-            if (execlp(command, command, NULL) == -1) {
-                // If exec fails, print an error message
-                perror("Command not found");
-                exit(EXIT_FAILURE);
-            }
-        } else if (pid > 0) {
-            // Parent process
-            int status;
-            waitpid(pid, &status, 0); // Wait for the child process to finish
-        } else {
-            // Forking error
-            perror("Fork error");
+        if (strcmp(command, "exit") == 0) {
+            printf("Exiting the shell...\n");
+            break;
         }
+
+        // Handle other commands here
+        // ...
     }
 
     return 0;
